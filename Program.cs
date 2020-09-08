609afd14c904f8b2f12917e3cb0667d3af73b6af
dmichaeltravis@hotmail.com <dmichaeltravis@hotmail.com>
2020-09-08 10:18:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldIntegration0908
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
